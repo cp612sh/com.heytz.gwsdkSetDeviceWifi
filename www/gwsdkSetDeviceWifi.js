b1f85ade52d4df74a1cfdc3b8d3f7f3c1500332e
Michael Chai <michaelchai@infapp.com>
2015-09-05 14:16:46 +0800

    Init

--- www/gwsdkSetDeviceWifi.js
+++ www/gwsdkSetDeviceWifi.js
@@ -4,5 +4,5 @@
 var exec = require('cordova/exec');
 
 exports.setDeviceWifi = function (ssid, wifikey, appId, success, error) {
-    exec(success, error, "gwsdkwrapper", "setDeviceWifi", [ssid, wifikey, appId]);
+    exec(success, error, "gwsdkSetDeviceWifi", "setDeviceWifi", [ssid, wifikey, appId]);
 };
